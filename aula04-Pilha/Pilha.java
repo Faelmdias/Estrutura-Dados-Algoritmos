29048d88c2471c62924002ce6e2efe5675ec890e
Faelmdias <168488728+Faelmdias@users.noreply.github.com>
2025-09-10 00:18:52 +0000

    pilha e fila

--- aula04-Pilha/Pilha.java
+++ aula04-Pilha/Pilha.java
@@ -1,4 +1,53 @@
-public class Pilha {
+public class Pilha<T> {
+    
+    private No<T> topo;
+    private String nomePilha;
 
+    public Pilha(){
+        this("");
+    }
+
+    public Pilha(String nomePilha){
+        this.nomePilha = nomePilha;
+        this.topo = null;
+    }
+
+    public void push(T dado){
+        No<T> novoNo = new No<T>(dado);
+        novoNo.setNextNo(topo);
+        topo = novoNo;
+    }
+    
+    public T peek(){
+        if(topo == null){
+            System.out.println("Pilha Vazia!");
+            return null;
+        }else{
+            return topo.getDado();
+        }
+    }
+
+    public T pop(){
+        if(topo == null){
+            System.out.println("Pilha Vazia!");
+            return null;
+        }else{
+            T dadoTemp = topo.getDado();
+            topo = topo.getNextNo();
+            return dadoTemp;
+        }
+    }
+
+    public void imprimePilha(){
+        if (topo == null) {
+            System.out.println("Pilha Vazia!");
+        } else {
+            No<T> aux = topo;
+            while (aux != null) { 
+                System.out.println(aux.getDado());
+                aux = aux.getNextNo();
+            }
+        }
+    }
     
 }
